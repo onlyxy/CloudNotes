5f28cfdc7aca532b20a97478f300fc168874df9b
XiYang <765849854@qq.com>
2016-02-03 14:25:57 +0800

    update

---
+++ app/src/main/java/com/yzh/nuc/cloudnotes/streetsdkfragment/PanoramaFragment.java
@@ -0,0 +1,51 @@
+package com.yzh.nuc.cloudnotes.streetsdkfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tencent.streetsdkdemo.R;
+import com.tencent.tencentmap.streetviewsdk.StreetViewPanoramaView;
+
+public class PanoramaFragment extends Fragment {
+	
+	private StreetViewPanoramaView mPanoramaView;	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.panarama_fragment, container, false);
+		mPanoramaView = (StreetViewPanoramaView)view.findViewById(R.id.panorama_view);
+		return view;
+	}
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+	
+	public PanoramaFragmentController getPanoramaFragmentController() {
+		return PanoramaFragmentController.getInstance(this);
+	}
+	
+	public StreetViewPanoramaView getStreetView() {
+		return mPanoramaView;
+	}
+}
