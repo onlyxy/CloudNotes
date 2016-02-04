bb3cc18daa4d9c0f8eef1bef15c8753bbc527a5c
xibiandetaiyang <765849854@qq.com>
2016-02-05 03:16:23 +0800

    Test

---
+++ Cloudnotes/src/main/java/com/cloudnotes/streetsdkdemo/ToastUtil.java
@@ -0,0 +1,11 @@
+package com.cloudnotes.streetsdkdemo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+	public static void showLongToast(Context context, String text) {
+		Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+		toast.show();
+	}
+}
