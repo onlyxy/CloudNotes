d4f4b20c8a19bd3c5e7f36032b94ca37862a3fb2
xibiandetaiyang <765849854@qq.com>
2016-02-18 21:41:57 +0800

    传感器test

--- Cloudnotes/src/main/java/com/cloudnotes/sensor/Gradienter.java
+++ Cloudnotes/src/main/java/com/cloudnotes/sensor/Gradienter.java
@@ -24,21 +24,121 @@ public class Gradienter extends Activity implements SensorEventListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.gradienter);
         //获取水平仪的主组件
         show = (MyView) findViewById(R.id.show);
         //获取传感器管理服务
-        mSensorManager = (SensorManager)
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        //为系统的方向传感器注册监听器
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+    }
 
+    @Override
+    protected void onPause() {
+        //取消注册
+        mSensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
     public void onSensorChanged(SensorEvent event) {
+        float[] valuse = event.values;
+        //获取触发event的传感器类型
+        int sensorType = event.sensor.getType();
+        switch (sensorType) {
+            case Sensor.TYPE_ORIENTATION:
+                //获取与Y轴的夹角
+                float yAngle = valuse[1];
+                //获取与Y轴的夹角
+                float zAngle = valuse[2];
+                //气泡位于中间时(水平仪完全水平),气泡的X、Y坐标
+                int x = (show.back.getWidth() - show.bubble.getWidth()) / 2;
+                int y = (show.back.getHeight() - show.bubble.getHeight()) / 2;
+                //如果与Z轴的倾斜角还在最大角度之内
+                if (Math.abs(zAngle) <= MAX_ANGLE) {
+                    //根据与Z轴的倾斜角度计算X坐标的变化值
+                    //（倾斜角度越大，X坐标变化大)
+                    int deltaX = (int) ((show).back.getWidth() - show.bubble.getWidth() / 2 * zAngle / MAX_ANGLE);
+                    x += deltaX;
+                }
+                //如果与Z轴的倾斜已经大于MAX_ANGLE，气泡应到最右边
+                else if (zAngle > MAX_ANGLE) {
+                    x = 0;
+                }
+                //如果与Z轴的倾斜角已经小于负的MAX_ANGLE，气泡应到最右边
+                else {
+                    x = show.back.getWidth() - show.bubble.getWidth();
+                }
+                //如果与Y轴的倾斜角还在最大角度之内
+                if (Math.abs(yAngle) <= MAX_ANGLE) {
+                    //根据与Y轴的倾斜角度计算Y坐标的变化值
+                    //（倾斜角度越大，Y坐标变化越大）
+                    int deltaX = (int) ((show.back.getHeight() - show.bubble.getHeight()) / 2 * yAngle / MAX_ANGLE);
+                    x += deltaX;
+                }
+                //如果与Z轴的倾斜角已经大于MAX_ANGLE，气泡欧应到最左边
+                else if (zAngle > MAX_ANGLE) {
+                    x = 0;
+                }
+                //如果与Z轴的倾斜角小于负的MAX_ANGLE，气泡应到最右边
+                else {
+                    x = show.back.getWidth() - show.bubble.getWidth();
+                }
+                //如果与Y轴的倾斜触角还在最大角度之内
+                if (Math.abs(yAngle) <= MAX_ANGLE) {
+                    //根据与Y轴的倾斜角度计算与Y坐标的变化值
+                    //（倾斜角度越大，Y坐标变化越大）
+                    int deltaY = (int) ((show.back.getHeight() - show.bubble.getHeight()) / 2 * yAngle / MAX_ANGLE);
+                    y += deltaY;
+                }
+                //如果与Y轴的倾斜角已经大于MAX_ANGLE，气泡应到最下边
+                else if (yAngle > MAX_ANGLE) {
+                    y = show.back.getHeight() - show.bubble.getHeight();
+                }
+                //如果与Y轴的倾斜角已经小于负的MAX_ANGLE，气泡应到最小边
+                else {
+                    y = 0;
+                }
+                //如果计算出来的X、Y坐标还位于水平仪的仪表盘内，则更新水平仪的气泡坐标
+                if (isContain(x, y)) {
+                    show.bubble
+                }
 
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    private boolean isContain(int x, int y) {
+        //计算气泡的圆心坐标X、Y
+        int bubbleCx = x + show.bubble.getWidth() / 2;
+        int bubbleCy = y + show.bubble.getWidth() / 2;
+        //计算水平仪仪表盘的圆心坐标X、Y
+        int backCx = show.back.getWidth() / 2;
+        int backCy = show.back.getWidth() / 2;
+        //计算气泡的圆心与水平仪仪表盘的圆心之间的距离
+        double distance = Math.sqrt((bubbleCx - backCx) * (bubbleCx - backCx) + (bubbleCy - backCx) * (bubbleCy - backCy));
+        //若两个圆忙的距离小于它们的半径差，即可认为处于该点的气泡依然位于仪表盘内
+        if (distance < (show.back.getWidth() - show.bubble.getWidth())) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
