7dcc7bb547cb302b72515f872d3ab6e2b8a14b3e
xibiandetaiyang <765849854@qq.com>
2016-02-19 02:35:11 +0800

    CloudNotes更新及优化

---
+++ Cloudnotes/src/main/java/com/nuc/yzh/cloudnotes/notes/AtyPhotoViewer.java
@@ -0,0 +1,37 @@
+package com.nuc.yzh.cloudnotes.notes;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.File;
+
+/**
+ * 显示照片的Activity
+ *
+ * @author XiYang
+ *
+ */
+public class AtyPhotoViewer extends Activity {
+
+    private ImageView iv;
+
+    public static final String EXTRA_PATH = "path";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        iv = new ImageView(this);
+        setContentView(iv);
+
+        String path = getIntent().getStringExtra(EXTRA_PATH);
+        if (path != null) {
+            iv.setImageURI(Uri.fromFile(new File(path)));
+        } else {
+            finish();
+        }
+    }
+
+}
---
+++ Cloudnotes/src/main/java/com/nuc/yzh/cloudnotes/notes/AtyVideoViewer.java
@@ -0,0 +1,37 @@
+package com.nuc.yzh.cloudnotes.notes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+/**
+ * Author:XiYang on 2016/2/18.
+ * Email:765849854@qq.com
+ *
+ * 显示视频的Activity
+ */
+
+public class AtyVideoViewer extends Activity {
+
+    private VideoView vv;
+
+    public static final String EXTRA_PATH = "path";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        vv = new VideoView(this);
+        vv.setMediaController(new MediaController(this));
+        setContentView(vv);
+
+        String path = getIntent().getStringExtra(EXTRA_PATH);
+        if (path != null) {
+            vv.setVideoPath(path);
+        } else {
+            finish();
+        }
+    }
+
+}
